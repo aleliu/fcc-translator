0650a8dbb08f41d28016ee7f778598da10b4fd03
aleliu <aleliug@gmail.com>
2025-03-21 20:43:42 -0400

    feat: unit tests

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,9 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    translate(text, type, highlight=true){
 
+    }
 }
 
 module.exports = Translator;
