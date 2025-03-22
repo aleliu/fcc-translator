2b2e17cc4fdd1c57004e838648879e987d0f100e
aleliu <aleliug@gmail.com>
2025-03-21 20:16:25 -0400

    feat: skeleton

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,15 +9,51 @@ let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
     test("Translation with text and locale fields: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post('/api/translate')
+            .send({text: "", locale: ""})
+            .end((err, res) => {
+            });
     })
     test("Translation with text and invalid locale field: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post('/api/translate')
+            .send({text: "", locale: ""})
+            .end((err, res) => {
+            });
     })
     test("Translation with missing text field: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post('/api/translate')
+            .send({text: "", locale: ""})
+            .end((err, res) => {
+            });
     })
     test("Translation with missing locale field: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post('/api/translate')
+            .send({text: "", locale: ""})
+            .end((err, res) => {
+            });
     })
     test("Translation with empty text: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post('/api/translate')
+            .send({text: "", locale: ""})
+            .end((err, res) => {
+            });
     })
     test("Translation with text that needs no translation: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post('/api/translate')
+            .send({text: "", locale: ""})
+            .end((err, res) => {
+            });
     })
 });
